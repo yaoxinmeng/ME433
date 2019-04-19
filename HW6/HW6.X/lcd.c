ed1bd4191c5cb12c6fb5d867569b6b1937dcfa1a
yaoxinmeng <yaoxinmeng@gmail.com>
2019-04-18 23:57:09 -0500

    initial

---
+++ HW6/HW6.X/lcd.c
@@ -0,0 +1,333 @@
+#include <xc.h>
+#include "lcd.h"
+
+static unsigned short bar_height, bar_width;
+
+void LCD_init() {
+    int time = 0;
+    
+    CS = 0; // CS
+   
+    LCD_command(ILI9341_SWRESET);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 7200000) {} // 300ms
+
+    LCD_command(0xEF);
+  	LCD_data(0x03);
+	LCD_data(0x80);
+	LCD_data(0x02);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xCF);
+  	LCD_data(0x00);
+	LCD_data(0xC1);
+	LCD_data(0x30);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xED);
+  	LCD_data(0x64);
+	LCD_data(0x03);
+	LCD_data(0x12);
+    LCD_data(0x81);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xE8);
+  	LCD_data(0x85);
+	LCD_data(0x00);
+	LCD_data(0x78);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xCB);
+  	LCD_data(0x39);
+	LCD_data(0x2C);
+	LCD_data(0x00);
+    LCD_data(0x34);
+    LCD_data(0x02);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xF7);
+  	LCD_data(0x20);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xEA);
+  	LCD_data(0x00);
+	LCD_data(0x00);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_PWCTR1);
+  	LCD_data(0x23);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_PWCTR2);
+  	LCD_data(0x10);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_VMCTR1 );
+  	LCD_data(0x3e);
+    LCD_data(0x28);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_VMCTR2);
+  	LCD_data(0x86);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_MADCTL);
+  	LCD_data(0x48);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+/*    
+    LCD_command(ILI9341_VSCRSADD);
+  	LCD_data(0x00);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+ */   
+    LCD_command(ILI9341_PIXFMT);
+  	LCD_data(0x55);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_FRMCTR1);
+  	LCD_data(0x00);
+    LCD_data(0x18);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command( ILI9341_DFUNCTR);
+  	LCD_data(0x08);
+    LCD_data(0x82);
+    LCD_data(0x27);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xF2);
+  	LCD_data(0); // 1
+    LCD_data(0x00);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_GAMMASET);
+  	LCD_data(0x01);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_GMCTRP1);
+  	LCD_data(0x0F);
+    LCD_data(0x31);
+    LCD_data(0x2B);
+    LCD_data(0x0C);
+    LCD_data(0x0E);
+    LCD_data(0x08);
+    LCD_data(0x4E);
+    LCD_data(0xF1);
+    LCD_data(0x37);
+    LCD_data(0x07);
+    LCD_data(0x10);
+    LCD_data(0x03);
+    LCD_data(0x0E);
+    LCD_data(0x09);
+    LCD_data(0x00);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_GMCTRN1);
+  	LCD_data(0x00);
+    LCD_data(0x0E);
+    LCD_data(0x14);
+    LCD_data(0x03);
+    LCD_data(0x11);
+    LCD_data(0x07);
+    LCD_data(0x31);
+    LCD_data(0xC1);
+    LCD_data(0x48);
+    LCD_data(0x08);
+    LCD_data(0x0F);
+    LCD_data(0x0C);
+    LCD_data(0x31);
+    LCD_data(0x36);
+    LCD_data(0x0F);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(0xB1);
+  	LCD_data(0x00);
+    LCD_data(0x10);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_SLPOUT);
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    LCD_command(ILI9341_DISPON);
+    
+    CS = 1; // CS
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    
+    CS = 0; // CS
+    
+    LCD_command(ILI9341_MADCTL);
+    LCD_data(MADCTL_MX | MADCTL_BGR); // rotation
+    time = _CP0_GET_COUNT();
+    while (_CP0_GET_COUNT() < time + 3600000) {} // 150ms
+    
+    CS = 1; // CS
+}
+
+void SPI1_init() {
+  SDI1Rbits.SDI1R = 0b0100; // B8 is SDI1
+  RPA1Rbits.RPA1R = 0b0011; // A1 is SDO1
+  TRISBbits.TRISB7 = 0; // CS is B7
+  CS = 1; // CS starts high
+
+  // DC pin
+  TRISBbits.TRISB15 = 0;
+  DC = 1;
+  
+  SPI1CON = 0; // turn off the spi module and reset it
+  SPI1BUF; // clear the rx buffer by reading from it
+  SPI1BRG = 0; // baud rate to 12 MHz [SPI1BRG = (48000000/(2*desired))-1]
+  SPI1STATbits.SPIROV = 0; // clear the overflow bit
+  SPI1CONbits.CKE = 1; // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1; // master operation
+  SPI1CONbits.ON = 1; // turn on spi1
+}
+
+unsigned char spi_io(unsigned char o) {
+  SPI1BUF = o;
+  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+    ;
+  }
+  return SPI1BUF;
+}
+
+void LCD_command(unsigned char com) {
+    DC = 0; // DC
+    spi_io(com);
+    DC = 1; // DC
+}
+
+void LCD_data(unsigned char dat) {
+    spi_io(dat);
+}
+
+void LCD_data16(unsigned short dat) {
+    spi_io(dat>>8);
+    spi_io(dat);
+}
+
+void LCD_setAddr(unsigned short x, unsigned short y, unsigned short w, unsigned short h) {
+    LCD_command(ILI9341_CASET); // Column
+    LCD_data16(x);
+	LCD_data16(x+w-1);
+
+	LCD_command(ILI9341_PASET); // Page
+	LCD_data16(y);
+	LCD_data16(y+h-1);
+
+	LCD_command(ILI9341_RAMWR); // Into RAM
+}
+
+void LCD_drawPixel(unsigned short x, unsigned short y, unsigned short color) {
+  // check boundary
+    if (x < 240 && y < 320){
+        CS = 0; // CS
+    
+        LCD_setAddr(x,y,1,1);
+        LCD_data16(color);
+    
+        CS = 1; // CS
+    }
+}
+
+void LCD_clearScreen(unsigned short color) {
+    int i;
+    
+    CS = 0; // CS
+    
+    LCD_setAddr(0,0,ILI9341_TFTWIDTH,ILI9341_TFTHEIGHT);
+	for (i = 0;i < ILI9341_TFTWIDTH*ILI9341_TFTHEIGHT; i++){
+		LCD_data16(color);
+	}
+    
+    CS = 1; // CS
+}
+
+void LCD_drawChar(char c, unsigned short x, unsigned short y, unsigned short f_color, unsigned short b_color){
+    unsigned short col, row;
+    for (col = 0; col < 5; col++){
+        char byte_col = ASCII[c - 0x20][col];   // get the byte of each column
+        for (row = 0; row < 8; row++){
+            if (((byte_col >> row) & 1) == 1){           // get the bit on that row
+                LCD_drawPixel(x+col, y+row, f_color);  // draw in font colour      
+            }
+            else{
+                LCD_drawPixel(x+col, y+row, b_color);   // draw in background colour
+            }
+        }
+    }
+}
+
+void LCD_print(char* m, unsigned short x, unsigned short y, unsigned short f_color, unsigned short b_color){
+    int i = 0;
+    unsigned short x_char;
+    while(m[i] != 0){
+        // position of new character
+        x_char = x + i*5;   
+        LCD_drawChar(m[i], x_char, y, f_color, b_color);
+        i++;
+    }
+}
+
+void LCD_makeBar(unsigned short x, unsigned short y, unsigned short height, unsigned short width, unsigned short f_color, unsigned short b_color){
+    unsigned short row, col;
+    // draw left and right margins
+    for(col = 0; col < 2; col++){
+        for(row = 0; row < height; row++){   
+            LCD_drawPixel(x+col, y+row, f_color);
+            LCD_drawPixel(x+col+width-2, y+row, f_color);
+        }
+    }
+    // draw top and bottom margins
+    for(col = 0; col < width; col++){
+        for(row = 0; row < 2; row++){   
+            LCD_drawPixel(x+col, y+row, f_color);
+            LCD_drawPixel(x+col, y+row+height-2, f_color);
+        }
+    }
+    // draw middle section of background color
+    for (col = 2; col < (width-4); col++){
+        for (row = 2; row < (height-4); row++){
+            LCD_drawPixel(x+col, y+row, b_color);
+        }
+    }
+    // set bar_height and bar_width for LCD_drawBar
+    bar_height = height - 4;
+    bar_width = width - 4;
+}
+
+void LCD_drawBar(unsigned short x, unsigned short y, unsigned short length, unsigned short f_color, unsigned short b_color){
+    unsigned short col, row;
+    for(col = 0; col < bar_width; col++){
+        for(row = 0; row < bar_height; row++){
+            if (col < length){
+                LCD_drawPixel(x+col, y+row, f_color);
+            }
+            else{
+                LCD_drawPixel(x+col, y+row, b_color);
+            }
+        }
+    }
+}
---
+++ HW6/HW6.X/main.c
@@ -0,0 +1,39 @@
+#include<math.h>        // needed for sin wave
+#include"startup.h"     // general initialisation code
+#include"lcd.h"         // lcd helper file
+
+#define BACKGROUND ILI9341_DARKGREEN
+#define FONT ILI9341_ORANGE
+#define INTERVAL 4800000    // 48MHz/10Hz = 4800000
+#define LENGTH 100          // counter length, also affects bar length
+#define BAR_HEIGHT 9        // bar height with border of 2px
+
+int main(void) {
+    init();
+    SPI1_init(); 
+    LCD_init();
+    LCD_clearScreen(BACKGROUND);     // reset background
+    
+    // define bar boundary
+    LCD_makeBar(28, 42, BAR_HEIGHT, LENGTH+4, ILI9341_BLACK, BACKGROUND);
+    char m[100];
+    unsigned char i = 0;
+    float fps = 0;
+    
+    while(1) {
+        _CP0_SET_COUNT(0);
+        sprintf(m, "Hello world %d!", i);
+        LCD_print(m, 28, 32, FONT, BACKGROUND);     // print text
+        LCD_drawBar(30, 44, i, FONT, BACKGROUND);   // print bar
+        i++;
+        if(i == LENGTH){       // reset i to loop back to 0
+            i = 0;
+            LCD_print(m, 28, 32, BACKGROUND, BACKGROUND);  // clear text
+        }
+        fps = 1/(((float)_CP0_GET_COUNT())/48000000);
+        sprintf(m, "FPS: %f", fps);
+        LCD_print(m, 28, 100, FONT, BACKGROUND);    // print fps
+        while(_CP0_GET_COUNT() < INTERVAL) {;}      // wait 1ms
+    }
+  return 0;
+}
